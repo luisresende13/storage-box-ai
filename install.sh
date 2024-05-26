b28f6df9d337293c5649a0b1798c84dcc88edd54
luisresende13 <luisresende13@gmail.com>
2024-05-26 18:49:24 -0300

    Add installation instructions

---
+++ install.sh
@@ -0,0 +1,13 @@
+# Installation steps:
+
+# CLone github repository
+git clone https://github.com/luisresende13/storage-box-ai.git
+
+# Change directory
+cd storage-box-ai
+
+# Install requirements
+pip install -r requirements.txt
+
+# Start application
+python main.py
